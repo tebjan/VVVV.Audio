a9d948221e98af1665a4d1d39505591b17ed6018
Tebjan Halm <tebjan@vvvv.org>
2013-10-06 03:58:10 +0200

    renamings and extracted some classes

---
+++ Source/Core/AudioEngine.cs
@@ -0,0 +1,130 @@
+﻿#region usings
+using System;
+using System.Collections.Generic;
+
+using NAudio.Wave;
+using NAudio.Wave.Asio;
+using NAudio.CoreAudioApi;
+using NAudio.Wave.SampleProviders;
+using NAudio.Utils;
+
+
+using VVVV.Core.Logging;
+#endregion usings
+
+namespace VVVV.Nodes
+{
+		
+	public enum AudioOutType
+	{
+		Asio,
+		Wasapi,
+		DirectSound,
+		Wave
+	}
+	
+	
+	public class AudioEngine: IDisposable, IStartable
+	{
+		public AudioEngine()
+		{
+			var format = WaveFormat.CreateIeeeFloatWaveFormat(44100, 1);
+			MultiInputProvider = new MultipleSampleToWaveProvider(format, () => OnFinishedReading());
+		}
+		
+		MultipleSampleToWaveProvider MultiInputProvider;
+		public AsioOut AsioOut;
+		
+		public event EventHandler FinishedReading;
+		
+		protected void OnFinishedReading()
+		{
+			var handle = FinishedReading;
+			if(handle != null)
+				handle(this, new EventArgs());
+		}
+		
+		#region asio
+		
+		public string DriverName
+		{
+			get;
+			set;
+		}
+		
+		//init driver
+	        public void CreateAsio()
+	        {
+	        	//recreate device if necessary
+	        	if (this.AsioOut != null)
+	        	{
+	        		Cleanup();
+	        	}
+	        	
+	        	this.AsioOut = new AsioOut(DriverName);
+	        	
+	        	this.AsioOut.Init(MultiInputProvider);
+	        }
+	        
+		//close
+		public void Dispose()
+		{
+			Cleanup();
+		}
+		
+		//close ASIO
+		private void Cleanup()
+		{
+			if (this.AsioOut != null)
+			{
+				this.AsioOut.Dispose();
+				this.AsioOut = null;
+			}
+		}
+		
+		#region IStatable
+		//Needed for IStartable. Do nothing right now...
+	        public void Start()	{}
+	
+	        //Shutdown engine when vvvv ends so vvvv process isn't left running
+	        public void Shutdown()
+		{ Cleanup(); }
+		#endregion IStatable		
+		
+		#endregion asio
+		
+		public void AddOutput(ISpread<AudioSignal> provider)
+		{
+			if(provider == null) return;
+			foreach(var p in provider)
+				MultiInputProvider.Add(p);
+		}
+		
+		public void RemoveOutput(ISpread<AudioSignal> provider)
+		{
+			if(provider == null) return;
+			foreach(var p in provider)
+				MultiInputProvider.Remove(p);
+		}
+	}
+	
+	public static class AudioService
+	{
+		private static AudioEngine FAudioEngine;
+		
+		public static AudioEngine Engine
+		{
+			get
+			{
+				return FAudioEngine;
+			}
+		}
+		
+		static AudioService()
+		{
+			FAudioEngine = new AudioEngine();
+		}		
+	}
+}
+
+
---
+++ Source/Core/MultipleSampleToWaveProvider.cs
@@ -0,0 +1,112 @@
+﻿#region usings
+using System;
+using System.ComponentModel.Composition;
+using System.Collections.Generic;
+
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+using VVVV.Utils.VColor;
+using VVVV.Utils.VMath;
+
+using NAudio.Wave;
+using NAudio.Wave.Asio;
+using NAudio.CoreAudioApi;
+using NAudio.Wave.SampleProviders;
+using NAudio.Utils;
+
+
+using VVVV.Core.Logging;
+#endregion usings
+
+namespace VVVV.Nodes
+{
+		/// <summary>
+	/// Helper class for when you need to convert back to an IWaveProvider from
+	/// an ISampleProvider. Keeps it as IEEE float
+	/// </summary>
+	public class MultipleSampleToWaveProvider : IWaveProvider
+	{
+	    private List<ISampleProvider> source = new List<ISampleProvider>();
+			private Action FReadingFinished;
+		
+	    /// <summary>
+	    /// Initializes a new instance of the WaveProviderFloatToWaveProvider class
+	    /// </summary>
+	    /// <param name="source">Source wave provider</param>
+	    public MultipleSampleToWaveProvider(WaveFormat format, Action readingFinished)
+	    {
+	    	this.WaveFormat = format;
+	    	this.FReadingFinished = readingFinished;
+	    }
+		
+		public void Add(ISampleProvider provider)
+		{
+			lock(source)
+			{
+				source.Add(provider);
+			}
+		}
+		
+		public void Remove(ISampleProvider provider)
+		{
+			lock(source)
+			{
+				source.Remove(provider);
+			}
+		}
+		
+	    /// <summary>
+	    /// Reads from this provider
+	    /// </summary>
+		float[] FMixerBuffer = new float[1];
+		
+		//this is called from the soundcard
+	    public int Read(byte[] buffer, int offset, int count)
+	    {
+	        int samplesNeeded = count / 4;
+	        WaveBuffer wb = new WaveBuffer(buffer);
+	    	
+	    	//fix buffer size
+	    	FMixerBuffer = BufferHelpers.Ensure(FMixerBuffer, samplesNeeded);
+	    	
+	    	//empty buffer
+	    	wb.Clear();
+	    	
+	    	lock(source)
+	    	{
+	    		var inputCount = source.Count;
+	    		//var invCount = 1.0f/inputCount;
+	    		for(int i=0; i<inputCount; i++)
+	    		{
+	    			if(source[i] != null)
+	    			{
+	    				//starts the calculation of the audio graph
+	    				source[i].Read(FMixerBuffer, offset / 4, samplesNeeded);
+	    				
+	    				//add to output buffer
+	    				for(int j=0; j<samplesNeeded; j++)
+	    				{
+	    					wb.FloatBuffer[j] += FMixerBuffer[j];
+	    					FMixerBuffer[j] = 0;
+	    				}
+	    			}
+	    		}
+	    		
+	    		//tell  the engine that reading has finished
+					FReadingFinished();
+	    	}
+	        return count; //always run 
+	    }
+	
+	    /// <summary>
+	    /// The waveformat of this WaveProvider (same as the source)
+	    /// </summary>
+	    public WaveFormat WaveFormat
+	    {
+	        get;
+	    	protected set;
+	    }
+	}
+}
+
+
