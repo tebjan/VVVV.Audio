d9995bb73884500321e4f429ba636c739a7769fe
tebjan <tebjan@vvvv.org>
2014-10-06 23:16:32 +0200

    added 50 test renderer

---
+++ AudioRendererTester/MainForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: TF
+ * Date: 06.10.2014
+ * Time: 22:48
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace AudioRendererTester
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Designer variable used to keep track of non-visual components.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        
+        /// <summary>
+        /// Disposes resources used by the form.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) {
+                if (components != null) {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+        
+        /// <summary>
+        /// This method is required for Windows Forms designer support.
+        /// Do not change the method contents inside the source code editor. The Forms designer might
+        /// not be able to load this method if it was changed manually.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Set Function";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Null Function";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(109, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 43);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Set Function 2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(211, 115);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "AudioRendererTester";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainFormFormClosed);
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ Source/vvvv50/AudioRenderer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VVVV.Audio
+{
+    public class SampleCallerSignal : AudioSignal
+    {
+
+        public Func<double, int, float> PerSample;
+
+        double FTime = 0;
+        int FSampleNumber = 0;
+        protected override void FillBuffer(float[] buffer, int offset, int count)
+        {
+            var increment = 1.0/SampleRate;
+            
+            if(PerSample != null)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    //calc sample
+                    buffer[i] = PerSample(FTime, FSampleNumber);
+                    
+                    //increment
+                    FTime += increment;
+                    FSampleNumber++;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    buffer[i] = 0;
+                    
+                    //increment
+                    FTime += increment;
+                    FSampleNumber++;
+                }
+            }
+        }
+    }
+    
+    public class AudioRender : IDisposable
+    {
+        AudioEngine FEngine;
+        SampleCallerSignal FSignal = new SampleCallerSignal();
+        List<MasterChannel> FMasterChannels = new List<MasterChannel>(2);
+		
+		public AudioRender()
+		{
+		    //setup engine
+			FEngine = AudioService.Engine;
+			FEngine.ChangeDriverSettings("ASIO4ALL v2", 44100, 2, 0, 2, 0);
+			
+			//create master channels
+			FMasterChannels.Add(new MasterChannel(FSignal, 0));
+			FMasterChannels.Add(new MasterChannel(FSignal, 1));
+			FEngine.AddOutput(FMasterChannels);
+			
+		    //start rendering
+			FEngine.Play = true;
+		}
+		
+		public double Render(Func<double, int, float> perSample)
+		{
+		    FSignal.PerSample = perSample;
+		    return FEngine.Timer.Time;
+		}
+
+		#region IDisposable implementation
+
+		public void Dispose()
+		{
+		    FEngine.Dispose();
+		}
+
+		#endregion
+    }
+}
